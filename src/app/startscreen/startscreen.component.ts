505edadbfc9945d345ed1482ad01e385115af9d3
MarkusLauber <lauber.m@gmx.net>
2022-02-19 17:33:20 +0100

    test

---
+++ src/app/startscreen/startscreen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startscreen',
+  templateUrl: './startscreen.component.html',
+  styleUrls: ['./startscreen.component.scss']
+})
+export class StartscreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
